912d3335d6e2c644aacfdeb8574093ccb8a2469b
guboyzasa <rachata.wa@rmuti.ac.th>
2022-06-21 02:03:12 +0700

    edit profile

--- resources/views/e-commerce/profile-account.blade.php
+++ resources/views/e-commerce/profile-account.blade.php
@@ -232,7 +232,7 @@
                                                         @foreach ($order->orderDetails as $detail)
                                                             <tr class="item">
                                                                 <td class="product-name">
-                                                                    <a href="product.html">{{ $detail->product->name }}</a>
+                                                                     <a href="product.html">{{ $detail->product->name }}</a> 
                                                                     <strong class="product-quantity">×
                                                                         {{ $detail->amount }}</strong>
                                                                 </td>
@@ -673,8 +673,7 @@
                                                             @foreach ($order->orderDetails as $detail)
                                                                 <tr class="item">
                                                                     <td class="product-name">
-                                                                        <a
-                                                                            href="product.html">{{ $detail->product->name }}</a>
+                                                                        <a href="product.html">{{ $detail->product->name }}</a>
                                                                         <strong class="product-quantity">×
                                                                             {{ $detail->amount }}</strong>
                                                                     </td>
